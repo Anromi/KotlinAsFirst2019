1735bba018f58d699e056fc8471a2d68a13f3c62
Anromi <54954473+Anromi@users.noreply.github.com>
2019-09-17 18:33:34 +0300

    Исправил функцию, которая состоит из одной операции

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -114,9 +114,8 @@ fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minute
  * Сколько денег будет на счету через 3 года (с учётом сложных процентов)?
  * Например, 100 рублей под 10% годовых превратятся в 133.1 рубля
  */
-fun accountInThreeYears(initial: Int, percent: Int): Double {
-    return initial * (1.0 + percent / 100.0).pow(3.0)
-}
+fun accountInThreeYears(initial: Int, percent: Int): Double = initial * (1.0 + percent / 100.0).pow(3.0)
+
 
 
 /**
