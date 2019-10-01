945f90d4e777161cbc68c637cd95ca7a501ed723
Anromi <54954473+Anromi@users.noreply.github.com>
2019-10-01 22:34:44 +0300

    високостный год исправил

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -51,8 +51,8 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean {
  */
 fun daysInMonth(month: Int, year: Int): Int {
     return when {
-        (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0 && month == 2) -> 29
         month == 1 || month == 3 ||month == 5 || month == 7 || month == 8 -> 31
+        (year % 4 == 0 && year % 100 != 0 && month == 2) || (year % 400 == 0 && month == 2) -> 29
         month == 2 -> 28
         else -> 30
     }
