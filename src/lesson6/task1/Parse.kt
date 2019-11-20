6ea6be232578284202c0209f0caad82fd356cdd9
Anromi <54954473+Anromi@users.noreply.github.com>
2019-11-20 22:10:30 +0300

    plusMinus1

--- src/lesson6/task1/Parse.kt
+++ src/lesson6/task1/Parse.kt
@@ -193,18 +193,14 @@ fun bestHighJump(jumps: String): Int = TODO()
  * Про нарушении формата входной строки бросить исключение IllegalArgumentException
  */
 fun plusMinus(expression: String): Int {
-    try {
-        val parts = expression.split(" ")
-        var sum = parts[0].toInt()
-        require(expression.matches(Regex("""\d*(\d+\s\+?[-]?\s\d*)*""")) && expression != "") // кидает IllegalArgumentException()
-        for ((i1, i2) in parts.withIndex()) {
-            if (i2 == "+") sum += parts[i1 + 1].toInt()
-            if (i2 == "-") sum -= parts[i1 + 1].toInt()
-        }
-        return sum
-    } catch (e: IllegalAccessError) {
-        throw e
+    val parts = expression.split(" ")
+    var sum = parts[0].toInt()
+    require(expression.matches(Regex("""\d*(\d+\s\+?[-]?\s\d*)*""")) && expression != "") // кидает IllegalArgumentException()
+    for ((i1, i2) in parts.withIndex()) {
+        if (i2 == "+") sum += parts[i1 + 1].toInt()
+        if (i2 == "-") sum -= parts[i1 + 1].toInt()
     }
+    return sum
 }
 
 /**
